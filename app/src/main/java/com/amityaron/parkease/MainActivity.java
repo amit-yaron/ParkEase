7c0b524023c18cd73e4ecaed42fd547ffe7f38f9
amit-yaron <amityaron100@gmail.com>
2023-11-03 10:49:15 +0200

    Authentication System Built

--- app/src/main/java/com/amityaron/parkease/MainActivity.java
+++ app/src/main/java/com/amityaron/parkease/MainActivity.java
@@ -2,7 +2,12 @@ package com.amityaron.parkease;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.amityaron.parkease.auth.LoginActivity;
+import com.amityaron.parkease.auth.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToRegister(View view) {
+        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+    }
+
+    public void goToLogin(View view) {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
 }
